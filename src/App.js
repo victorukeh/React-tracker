8630546c0e6abb43dc7c233b23d00647fbef3ac1
victorukeh <victorukeh1.com>
2021-11-02 14:01:54 +0100

    Done with the task tracker

--- src/App.js
+++ src/App.js
@@ -1,54 +1,124 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ]);
-  // const name = "Victor";
-  // const x = true;
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
 
-  //Delete Tsdk
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
+  // useEffect is used when you want something to happen when the page loads
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+    // The [] is a dependency array where if you have
+    //  a value where you want this to run if the value changes
+  }, [])
 
-  //Toggle Remainder
-  const toggleRemainder = (id) => {
-   setTasks(tasks)
-  };
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  // Fetch Task (For the Toggle)
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+  //Add Task(sumbit)
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // //Add former tasks to new Task
+    // setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Task
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(updTask),
+    })
+    const data = await res.json()
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
+  }
 
   return (
-    <div className="container">
-      <Header />
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder} />
-      ) : (
-        "No task to show"
-      )}
-      {/* <Header title='Hello'/> */}
-      {/* <Header title={1}/>  Thus will give invalid prop title number*/}
-      {/* <h2>Hello {name}</h2>
-      <h3>Hello {x ? 'Yes' : 'No'}</h3> */}
-    </div>
-  );
-};
+    <Router>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+      {/* /To put the addTask and Task only in index route */}
+        <Route
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No tasks to show'
+              )}
+            </>
+          )}
+        />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
+  )
+}
 
-export default App;
+export default App
+
+// If serve in not working because of disable Javascript scripts
+// Run the following commands
+// Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+// Get-ExecutionPolicy
+// Get-ExecutionPolicy -list
+// ng --version  (for Angular)
+// Your problem should be solved after these commands
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,18 +1,21 @@
-import { FaTimes } from "react-icons/fa";
+import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task" onDoubleClick={() => onToggle(task.id)}>
+    <div
+    className={`task ${task.reminder && 'reminder'}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
       <h3>
-        {task.text}{" "}
-        <FaTimes style={{ color: "red", cursor: "point" }} 
-        onClick={() => onDelete(task.id)} />
+        {task.text}{' '}
+        <FaTimes
+          style={{ color: 'red', cursor: 'pointer' }}
+          onClick={() => onDelete(task.id)}
+        />
       </h3>
       <p>{task.day}</p>
     </div>
-  );
-};
+  )
+}
 
-//Actions get passed up and state gets passed down
-
-export default Task;
+export default Task
